4d9c923bf2d632422ea3e4c2564adfda0d9c22a1
Keita Ito <gi1okt@gmail.com>
2015-02-12 19:09:02 -0800

    Initial Commit

---
+++ NCCBF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NCCBF
+//
+//  Created by Keita on 2/12/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
