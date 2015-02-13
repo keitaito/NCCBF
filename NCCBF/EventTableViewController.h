f7540c4a85d443bbec4c51783e744fcdf5a071e3
Keita Ito <gi1okt@gmail.com>
2015-02-12 19:49:06 -0800

    added EventTableViewController

---
+++ NCCBF/EventTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventTableViewController.h
+//  NCCBF
+//
+//  Created by Keita on 2/12/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventTableViewController : UITableViewController
+
+@end
