bf616961fbe0aa371c177d7418ced5ff66e13a63
Keita Ito <gi1okt@gmail.com>
2015-03-06 17:39:20 -0800

    add AboutUsDetailViewController

---
+++ NCCBF/AboutUsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutUsDetailViewController.h
+//  NCCBF
+//
+//  Created by Keita on 3/6/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutUsDetailViewController : UIViewController
+
+@end
