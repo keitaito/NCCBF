00cfbfd098d599c65e1a2c9c6fe19722b931e907
Keita Ito <gi1okt@gmail.com>
2015-03-09 18:48:34 -0700

    add setupDefaultLocation method

--- NCCBF/MapViewController.m
+++ NCCBF/MapViewController.m
@@ -21,17 +21,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Set mapView delegate.
     self.mapView.delegate = self;
+    // Instantiate CLLocationManager and set delegate.
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     
-    #ifdef __IPHONE_8_0
-        if(IS_OS_8_OR_LATER) {
-            // Use one or the other, not both. Depending on what you put in info.plist
+    
+//    #ifdef __IPHONE_8_0
+//        if(IS_OS_8_OR_LATER) {
+//            // Use one or the other, not both. Depending on what you put in info.plist
             [self.locationManager requestWhenInUseAuthorization];
 //            [self.locationManager requestAlwaysAuthorization];
-        }
-    #endif
+//        }
+//    #endif
     
     [self.locationManager startUpdatingLocation];
     
@@ -45,6 +49,14 @@
 //        [self.locationManager requestWhenInUseAuthorization];
 //    }
 //    [self.locationManager startUpdatingLocation];
+    
+    
+    
+    
+    //////////
+    
+//    [self gotoDefaultLocation];
+    [self setupDefaultLocation];
 }
 
 -(void)viewDidAppear:(BOOL)animated {
@@ -53,40 +65,86 @@
     self.locationManager.distanceFilter = kCLDistanceFilterNone;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     [self.locationManager startUpdatingLocation];
-    NSLog(@"%@", [self deviceLocation]);
     
-    //View Area
-    MKCoordinateRegion region = { { 0.0, 0.0 }, { 0.0, 0.0 } };
-    region.center.latitude = self.locationManager.location.coordinate.latitude;
-    region.center.longitude = self.locationManager.location.coordinate.longitude;
-    region.span.longitudeDelta = 0.005f;
-    region.span.longitudeDelta = 0.005f;
-    [self.mapView setRegion:region animated:YES];
+//    NSLog(@"%@", [self deviceLocation]);
+    
+//    //View Area
+//    MKCoordinateRegion region = { { 0.0, 0.0 }, { 0.0, 0.0 } };
+//    region.center.latitude = self.locationManager.location.coordinate.latitude;
+//    region.center.longitude = self.locationManager.location.coordinate.longitude;
+//    region.span.longitudeDelta = 0.005f;
+//    region.span.longitudeDelta = 0.005f;
+//    [self.mapView setRegion:region animated:YES];
 }
 
-- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
-{
-    __unused MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
-//    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
-}
-- (NSString *)deviceLocation {
-    return [NSString stringWithFormat:@"latitude: %f longitude: %f", self.locationManager.location.coordinate.latitude, self.locationManager.location.coordinate.longitude];
-}
-- (NSString *)deviceLat {
-    return [NSString stringWithFormat:@"%f", self.locationManager.location.coordinate.latitude];
-}
-- (NSString *)deviceLon {
-    return [NSString stringWithFormat:@"%f", self.locationManager.location.coordinate.longitude];
-}
-- (NSString *)deviceAlt {
-    return [NSString stringWithFormat:@"%f", self.locationManager.location.altitude];
-}
+
+//- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+//{
+//    __unused MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+////    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+//}
+//- (NSString *)deviceLocation {
+//    return [NSString stringWithFormat:@"latitude: %f longitude: %f", self.locationManager.location.coordinate.latitude, self.locationManager.location.coordinate.longitude];
+//}
+//- (NSString *)deviceLat {
+//    return [NSString stringWithFormat:@"%f", self.locationManager.location.coordinate.latitude];
+//}
+//- (NSString *)deviceLon {
+//    return [NSString stringWithFormat:@"%f", self.locationManager.location.coordinate.longitude];
+//}
+//- (NSString *)deviceAlt {
+//    return [NSString stringWithFormat:@"%f", self.locationManager.location.altitude];
+//}
 
 //// Location Manager Delegate Methods
 //- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 //{
 //    NSLog(@"%@", [locations lastObject]);
 //}
+
+- (void)setupDefaultLocation {
+    
+    // Create Japantown location coordinates.
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Japantown" ofType:@"plist"];
+    NSDictionary *properties = [NSDictionary dictionaryWithContentsOfFile:filePath];
+    
+    CLLocationCoordinate2D midCoordinate;
+    CLLocationCoordinate2D topLeftCoordinate;
+    CLLocationCoordinate2D topRightCoordinate;
+    CLLocationCoordinate2D bottomLeftCoordinate;
+    CLLocationCoordinate2D bottomRightCoordinate;
+    
+    // Create 4 coordinates of Japantown.
+    CGPoint midPoint = CGPointFromString(properties[@"midCoord"]);
+    midCoordinate = CLLocationCoordinate2DMake(midPoint.x, midPoint.y);
+    
+    CGPoint topLeftPoint = CGPointFromString(properties[@"topLeftCoord"]);
+    topLeftCoordinate = CLLocationCoordinate2DMake(topLeftPoint.x, topLeftPoint.y);
+    
+    CGPoint topRightPoint = CGPointFromString(properties[@"topRightCoord"]);
+    topRightCoordinate = CLLocationCoordinate2DMake(topRightPoint.x, topRightPoint.y);
+    
+    CGPoint bottomLeftPoint = CGPointFromString(properties[@"bottomLeftCoord"]);
+    bottomLeftCoordinate = CLLocationCoordinate2DMake(bottomLeftPoint.x, bottomLeftPoint.y);
+    
+    bottomRightCoordinate = CLLocationCoordinate2DMake(bottomLeftCoordinate.latitude, topRightCoordinate.longitude);
+    
+    
+    // Create latitudeDelta.
+    CLLocationDegrees latDelta = topLeftCoordinate.latitude - bottomRightCoordinate.latitude;
+    NSLog(@"%f", latDelta);
+    
+    // think of a span as a tv size, measure from one corner to another
+    MKCoordinateSpan span = MKCoordinateSpanMake(fabsf(latDelta), 0.0);
+    NSLog(@"%f %f", span.latitudeDelta, span.longitudeDelta);
+    
+    // Create region with midCoordinate and span.
+    MKCoordinateRegion region = MKCoordinateRegionMake(midCoordinate, span);
+    
+    // set map view region.
+//    self.mapView.region = region;
+    [self.mapView setRegion:region animated:YES];
+}
 
 
 
