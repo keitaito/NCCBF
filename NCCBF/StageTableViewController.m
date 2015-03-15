b6c803c320da8c57cda07f191537d416397b592e
Keita Ito <gi1okt@gmail.com>
2015-03-14 20:43:23 -0700

    add value in methods: numberOfSection, numberOfRows, cellForRowAtIndexPath

--- NCCBF/StageTableViewController.m
+++ NCCBF/StageTableViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "StageTableViewController.h"
+#import "Event.h"
 
 @interface StageTableViewController ()
 
@@ -32,26 +33,28 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
+//#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
+//#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return self.eventsArray.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"StageTableViewCell" forIndexPath:indexPath];
     
     // Configure the cell...
+    Event *event = self.eventsArray[indexPath.row];
+    cell.textLabel.text = event.name;
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
