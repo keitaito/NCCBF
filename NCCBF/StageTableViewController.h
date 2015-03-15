d524ba9afdab48acf198c868e388d421ed411c59
Keita Ito <gi1okt@gmail.com>
2015-03-14 20:20:37 -0700

    add StageTableViewController

---
+++ NCCBF/StageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StageTableViewController.h
+//  NCCBF
+//
+//  Created by Keita on 3/14/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StageTableViewController : UITableViewController
+
+@end
