178e53194215f74760ace79db42c854501c91eb4
Keita Ito <gi1okt@gmail.com>
2015-03-21 17:34:40 -0700

    add AboutUsDetailViewCell

---
+++ NCCBF/AboutUsDetailViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AboutUsDetailViewCell.m
+//  NCCBF
+//
+//  Created by Keita on 3/21/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import "AboutUsDetailViewCell.h"
+
+@implementation AboutUsDetailViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
