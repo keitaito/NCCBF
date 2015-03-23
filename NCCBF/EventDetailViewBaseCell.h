ee33c647bcb947855d2fb147bb53b36ebe1c7afe
Keita Ito <gi1okt@gmail.com>
2015-03-22 17:02:02 -0700

    add EventDetailViewBaseCell

---
+++ NCCBF/EventDetailViewBaseCell.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewBaseCell.h
+//  NCCBF
+//
+//  Created by Keita on 3/22/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewBaseCell : UITableViewCell
+
+@end
---
+++ NCCBF/EventDetailViewBaseCell.m
@@ -0,0 +1,23 @@
+//
+//  EventDetailViewBaseCell.m
+//  NCCBF
+//
+//  Created by Keita on 3/22/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+#import "EventDetailViewBaseCell.h"
+
+@implementation EventDetailViewBaseCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
